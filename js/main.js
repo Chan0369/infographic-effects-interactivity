fb2611c8e31760161585e8500d73acf47a0a174c
Chan0369 <xbetty_chanx@outlook.com>
2016-04-22 14:32:47 -0400

    Fixed cupid animation and added animation to green hearts

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
-$('.nav').localScroll();
-
 var $cupids = $('.cupid-right, .cupid-left');
+
+$('.nav').localScroll();
 
 $cupids.waypoint(function (direction) {
   if (direction == 'down') {
@@ -16,4 +16,24 @@ $('.right-heart-info').on('click', function (e) {
 
 $('.left-heart-info').on('click', function (e) {
   $(this).toggleClass('is-clicked');
+});
+
+$('.mint-heart-left').on('click', function (e) {
+  $(this).addClass('is-clicked2');
+});
+
+$('mint-heart-left').on('animationend', function (e) {
+  $(this).removeClass('is-clicked2');
+});
+
+$('.mint-heart-right').on('click', function (e) {
+  $(this).addClass('is-clicked2');
+});
+
+$('mint-heart-right').on('animationend', function (e) {
+  $(this).removeClass('is-clicked2');
+});
+
+$('family').hover( function (e) {
+  $(this).addClass('scale');
 });
